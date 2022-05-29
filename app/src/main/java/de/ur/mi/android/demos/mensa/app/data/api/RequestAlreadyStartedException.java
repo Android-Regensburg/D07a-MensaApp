f151cdccfd6bb92a6ace3895f52d93f57e646a43
ragor114 <69634980+ragor114@users.noreply.github.com>
2022-05-29 16:25:46 +0200

    setup master branch for assignmentviewer

--- app/src/main/java/de/ur/mi/android/demos/mensa/app/data/api/RequestAlreadyStartedException.java
+++
@@ -1,7 +0,0 @@
-package de.ur.mi.android.demos.mensa.app.data.api;
-
-/**
- * Wird ausgelöst, wenn versucht wird, einen bereits gestarteten WeeklyMenuRequest erneut zu starten
- */
-public class RequestAlreadyStartedException extends Throwable {
-}
