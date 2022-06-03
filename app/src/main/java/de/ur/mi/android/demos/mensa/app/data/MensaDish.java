944eb07d1558d12c79559b4f0d79d75ef594b94f
ragor114 <69634980+ragor114@users.noreply.github.com>
2022-06-03 17:08:14 +0200

    removed fromJSON method

--- app/src/main/java/de/ur/mi/android/demos/mensa/app/data/MensaDish.java
+++ app/src/main/java/de/ur/mi/android/demos/mensa/app/data/MensaDish.java
@@ -22,14 +22,4 @@ public class MensaDish {
         this.onDay = onDay;
         this.category = category;
     }
-
-    /**
-     * Versucht, aus dem übergebenen JSONObject die notwendigen Werte zum Erstellen einer MensaDish-
-     * Instanz auszulesen und ein entsprechendes Objekt zurückzugeben.
-     */
-    public static MensaDish fromJSONObject(JSONObject object) throws JSONException {
-        // Implementieren Sie hier das Auslesen der Daten aus dem JSON Objekt.
-        // Erzeugen Sie mit den ausgelesenen Daten ein neues Objekt der Klasse MensaDish und geben Sie dieses zurück.
-        return null;
-    }
 }
